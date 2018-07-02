1a64e634d98cbaf85111c8a28b8d627284817c09
escolmebartlebooth <escolme@bartlebooth.co.uk>
2018-07-02 20:04:28 +0100

    initialise PID controller

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,6 +11,19 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  // set the initial gains and errors
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  this->p_error = 0;
+  this->i_error = 0;
+  this->d_error = 0;
+
+  cout << "PID Controller Initialised" << endl;
+  cout << this->Kp << " " << this->Ki << " " << this->Kd << endl;
+  cout << this->p_error << " " << this->i_error << " " << this->d_error << endl;
+
 }
 
 void PID::UpdateError(double cte) {
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,10 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  double iKp = 0.1
+  double iKi = 0.001
+  double iKd = 0.01
+  pid.init(iKp, iKi, iKd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,7 +61,7 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
